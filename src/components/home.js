49bef1458755efda6a20a5305135f6db1edbabea
mehdi <mohseninasab.mehdi@gmail.com>
2018-08-07 00:25:17 +0430

    add new componant to the home page which is awesome

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import {Alert} from 'react-bootstrap';
 import React from 'react';
+import Clock from './clock'
 
 
 
@@ -9,7 +10,8 @@ const Home = () =>{
 
         <div className="row text-center">
                 <div className="col-sm-12 col-md-8 col-lg-4 offset-md-2 offset-lg-4 p-0 mt-4">
-                <Alert bsStyle="success">∆∆ &emsp; this is Home page &emsp; ∆∆</Alert>
+                <Alert bsStyle="danger">∆∆ &emsp; this is Home page &emsp; ∆∆</Alert>
+                <Clock />
                 </div>
             </div>
     );
